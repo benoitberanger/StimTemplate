496e952666bcb6b118e3b90b19abaf50ea2cb880
tnd <benoit.beranger@icm-institute.org>
2016-01-06 21:16:06 +0100

    iscolumn doesn't exist on R2010a

--- @EventRecorder/EventRecorder.m
+++ @EventRecorder/EventRecorder.m
@@ -121,7 +121,7 @@ classdef EventRecorder < handle
             % Add event, according to the dimensions given by the Header
             
             if length( event ) == obj.Columns % Check input arguments
-                if iscolumn( event )
+                if size( event , 1 ) > 0 && size( event , 2 ) == 1 % if iscolumn( event )
                     event = event';
                 end
                 obj.IncreaseEventCount;
