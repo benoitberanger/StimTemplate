f098ea48854341e52af9dae6f2b37fc02e9830f3
Benoit BERANGER <benoit.beranger@icm-institute.org>
2016-04-06 17:16:08 +0200

    KbLogger.ScaleTime improvement : now we can use StartTime as input, mainly for debugging (Kb inputs + GenerateMRITrigg now have the same time reference)

--- @KbLogger/KbLogger.m
+++ @KbLogger/KbLogger.m
@@ -190,8 +190,8 @@ classdef KbLogger < EventRecorder
             
             for v = 1 : volumes
                 
-                obj.AddEvent({ MRI_trigger_tag (v-1)*tr                1 NaN })
-                obj.AddEvent({ MRI_trigger_tag (v-1)*tr+pulse_duration 0 NaN })
+                obj.AddEvent({ MRI_trigger_tag (v-1)*tr                1 (v-1)*tr                 })
+                obj.AddEvent({ MRI_trigger_tag (v-1)*tr+pulse_duration 0 (v-1)*tr+pulse_duration  })
                 
             end
             
@@ -278,6 +278,40 @@ classdef KbLogger < EventRecorder
             
             % Store curves
             obj.GraphData = obj.KbEvents;
+            
+        end
+        
+        % -----------------------------------------------------------------
+        %                             Scale Time
+        % -----------------------------------------------------------------
+        function ScaleTime( obj , StartTime )
+            % obj.ScaleTime()
+            %
+            % Scale the time origin to the first entry in obj.Data
+            
+            if nargin < 2
+                
+                
+            else
+                
+                % --- StartTime ----
+                if ~( isnumeric(StartTime) && StartTime > 0 )
+                    error('StartTime must be positive')
+                end
+                
+                % Add StartTime at the top of obj.Data
+                obj.Data = [ ...
+                    { obj.Header{1} StartTime       1 NaN } ; ...
+                    { obj.Header{1} StartTime+0.001 0 NaN } ; ...
+                    obj.Data...
+                    ];
+                obj.IncreaseEventCount;
+                obj.IncreaseEventCount;
+                
+            end
+            
+            % Apply upper class method
+            ScaleTime@EventRecorder(obj);
             
         end
         
