0481ad88895783c9b52e61979e96fd03d0761c84
Benoit BERANGER <benoit.beranger@icm-institute.org>
2016-05-26 14:50:34 +0200

    saveobj & loadobj : does not impact previous and future objects saved modifications or commented, maybe will be used in future...

--- @EventRecorder/EventRecorder.m
+++ @EventRecorder/EventRecorder.m
@@ -518,6 +518,52 @@ classdef EventRecorder < handle
             
         end
         
+        %         % -----------------------------------------------------------------
+        %         %                             saveobj
+        %         % -----------------------------------------------------------------
+        %         function s = saveobj(obj)
+        %
+        %             s = struct; % Create a sctructure
+        %             props = properties(obj);
+        %             for p = 1:numel(props)
+        %                 s.(props{p})=obj.(props{p}); % Fill the structer with the object properties
+        %             end
+        %
+        %         end
+        
     end % methods
+    
+    methods ( Static )
+        
+        %         % -----------------------------------------------------------------
+        %         %                             loadobj
+        %         % -----------------------------------------------------------------
+        %         function obj = loadobj(s)
+        %
+        %             if isstruct(s) % if the object was saved as a structure
+        %
+        %                 % Fetch class name
+        %                 [~, classsName, ~] = fileparts(s.Description);
+        %
+        %                 newObj = feval(str2func(classsName)); % Create an empty instance
+        %                 props = properties(newObj);
+        %                 for p = 1:numel(props)
+        %                     try
+        %                         newObj.(props{p})=s.(props{p}); % Fill the instance
+        %                     catch err
+        %                         warning(err.message)
+        %                     end
+        %                 end
+        %                 obj = newObj; % Assign the pointer
+        %
+        %             else % if the object was saved as an object
+        %
+        %                 obj = s;
+        %
+        %             end
+        %
+        %         end
+        
+    end % methods ( Static )
     
 end % class
