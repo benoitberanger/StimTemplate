6139753334ec61152873de491a911f455e7e2873
Benoit BERANGER <benoit.beranger@icm-institute.org>
2016-08-25 18:11:34 +0200

    PsedoRand2Conditions minor update : nodisplay

--- PsedoRand2Conditions.m
+++ PsedoRand2Conditions.m
@@ -1,15 +1,20 @@
-function [ Sequence ] = PsedoRand2Conditions( C0 , C1 )
+function [ Sequence ] = PsedoRand2Conditions( C0 , C1 , nodisplay )
 %PSEDORAND2CONDITIONS Generates a sequences of 0 and 1 where there is  no
 %two 1 in a row
 %
 %   C0 is the number of the 0 in the sequence
 %   C1 is the number of the 1 in the sequence
+%   nodisplay is 0 or 1 (flag)
 %
 %   WARNING : the method is adapted for C0|C1 not too big (<500|250) and
 %   for C1/C2~0.5.
 
 % benoit.beranger@icm-institute.org
 % CENIR-ICM , 2016
+
+if nargin < 3
+    nodisplay = 0;
+end
 
 maxiteration = 10000;
 
@@ -29,9 +34,12 @@ while ~exit_randomization
     if length(StreamRand) > L % enouth stream
         
         iteration = iteration + 1;
-        fprintf('iteration = %d \n',iteration)
         
-        for idx = 1:(length(StreamRand)-L) 
+        if ~nodisplay
+            fprintf('iteration = %d \n',iteration)
+        end
+        
+        for idx = 1:(length(StreamRand)-L)
             
             window = StreamRand(idx:idx+L-1); % sliding window inside the stream
             res = sum(window); % sum of 1
