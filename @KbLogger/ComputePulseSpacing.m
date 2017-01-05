0282885e196a39a3fa757cedf25d01e30b9ec5b9
Benoît Béranger <benoit.beranger@icm-institute.org>
2017-01-05 16:36:51 +0100

    minor improvement if KeyLogger.ComputePulseSpacing

--- @KbLogger/ComputePulseSpacing.m
+++ @KbLogger/ComputePulseSpacing.m
@@ -1,7 +1,11 @@
-function ComputePulseSpacing( obj )
-% obj.ComputePulseSpacing()
+function ComputePulseSpacing( obj , graph )
+% obj.ComputePulseSpacing() no plot, or obj.ComputePulseSpacing(1) to plot
 %
-% Compute time between each "KeyIsDown", then plot it
+% Compute time between each "KeyIsDown", then plot it if asked
+
+if ~exist('graph','var')
+    graph = 0;
+end
 
 for k = 1 : size(obj.KbEvents,1)
     
@@ -17,26 +21,31 @@ for k = 1 : size(obj.KbEvents,1)
         KeyIsDown_onset = data(KeyIsDown_idx,1);
         KeyIsDown_spacing = diff(KeyIsDown_onset);
         
+        fprintf('N = %d \n',length(KeyIsDown_onset));
         fprintf('mean = %f ms \n',mean(KeyIsDown_spacing)*1000);
         fprintf('std = %f ms \n',std(KeyIsDown_spacing)*1000);
         
-        figure( ...
-            'Name'        , [mfilename ' : ' obj.KbEvents{k,1} ] , ...
-            'NumberTitle' , 'off'                       , ...
-            'Units'       , 'Normalized'                , ...
-            'Position'    , [0.05, 0.05, 0.90, 0.80]      ...
-            )
-        
-        subplot(2,2,[1 2])
-        plot(KeyIsDown_spacing)
-        
-        subplot(2,2,3)
-        hist(KeyIsDown_spacing)
-        
-        if ~isempty(which('boxplot'))
-            subplot(2,2,4)
-            boxplot(KeyIsDown_spacing)
-            grid on
+        if graph
+            
+            figure( ...
+                'Name'        , [mfilename ' : ' obj.KbEvents{k,1} ] , ...
+                'NumberTitle' , 'off'                       , ...
+                'Units'       , 'Normalized'                , ...
+                'Position'    , [0.05, 0.05, 0.90, 0.80]      ...
+                )
+            
+            subplot(2,2,[1 2])
+            plot(KeyIsDown_spacing)
+            
+            subplot(2,2,3)
+            hist(KeyIsDown_spacing)
+            
+            if ~isempty(which('boxplot'))
+                subplot(2,2,4)
+                boxplot(KeyIsDown_spacing)
+                grid on
+            end
+            
         end
         
     end
