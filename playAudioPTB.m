93a083cc0b287381db6630e6807740fc144e6775
Benoît Béranger <benoit.beranger@icm-institute.org>
2017-02-09 13:02:10 +0100

    minor modif in playAudioPTB

--- playAudioPTB.m
+++ playAudioPTB.m
@@ -14,12 +14,16 @@ end
 nrchannels = 1;
 repetitions = 1;
 
-InitializePsychSound;
+InitializePsychSound(1);
 
 pahandle = PsychPortAudio('Open', [], [], 0, freq, nrchannels);
 
 PsychPortAudio('FillBuffer', pahandle, wavedata);
 
 PsychPortAudio('Start', pahandle, repetitions, 0, 1);
+
+PsychPortAudio('Stop', pahandle, 0, 1);
+
+PsychPortAudio('Close',pahandle);
 
 end
