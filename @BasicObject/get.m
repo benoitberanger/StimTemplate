200a2df23a2440d27b24f346dfe79ae4075b8572
Benoît Béranger <benoit.beranger@icm-institute.org>
2017-12-19 20:04:13 +0100

    bugfix in @BasicObject/get.m

--- @BasicObject/get.m
+++ @BasicObject/get.m
@@ -3,6 +3,7 @@ function [ output ] = get( self, evt, str )
 % Here, this method is a way to fetch an element in self.Data
 
 column = ~cellfun(@isempty,regexp(self.Header,str,'once'));
+assert(any(column), 'get method did not find ''%s'' in the Header', str)
 output = self.Data{evt,column};
 
 end % function
