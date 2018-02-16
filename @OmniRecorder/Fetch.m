94cbdf32109a9604a5ae7469925999b2c74b16f4
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-02-16 14:42:42 +0100

    @OmniRecorder/Fetch : better Help and errors for input arguments

--- @OmniRecorder/Fetch.m
+++ @OmniRecorder/Fetch.m
@@ -1,11 +1,17 @@
 function [ output ] = Fetch( self, str, column )
 %FETCH will fetch the lines containing 'str' in the column 'column'
+%
+% Exemple : self.Fetch( '+', self.Get('reward') )
+%
 % If 'column' is not defined, it will fetch in the culumn=1;
 % To easily get a column index, use self.Get('columnRegex') method
 
 if nargin < 3
     column = 1;
 end
+
+assert( nargin>=2 , 'str must be defined')
+assert( ischar(str) && isvector(str), 'str must be char' )
 
 try
     lines = ~cellfun(@isempty,regexp(self.Data(:,column),str,'once'));
