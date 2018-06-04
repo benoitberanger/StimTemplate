9a9b626e223c1b345828d0cfaf9017cf66d0a14c
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-06-04 20:44:36 +0200

    bug fix ? i didn't notice... or maybe it's not a bug, on KeyLoger.ScaleTime

--- @OmniRecorder/ScaleTime.m
+++ @OmniRecorder/ScaleTime.m
@@ -1,7 +1,11 @@
-function ScaleTime( self )
+function ScaleTime( self, t0 )
 % self.ScaleTime()
 %
 % Scale the time origin to the first entry in self.Data
+
+if nargin < 2
+    t0 = [];
+end
 
 % Onsets of events
 time = cell2mat( self.Data( : , 2 ) );
@@ -12,17 +16,24 @@ className = class(self);
 switch className
     case 'EventRecorder'
         column_to_write_scaled_onsets = 2;
+        value = time(1);
     case 'KbLogger'
         column_to_write_scaled_onsets = 4;
+        if ~isempty(t0)
+            value = t0;
+        else
+            value = time(1);
+        end
     case 'EventPlanning'
         column_to_write_scaled_onsets = 2;
+        value = time(1);
     otherwise
         error('Unknown object caller. Check self.Description')
 end
 
 % Write scaled time
 if ~isempty( time )
-    self.Data( : , column_to_write_scaled_onsets ) = num2cell( time - time(1) );
+    self.Data( : , column_to_write_scaled_onsets ) = num2cell( time - value );
 else
     warning( 'Recorder:ScaleTime' , 'No data in %s.Data (%s)' , inputname(1) , className )
 end
